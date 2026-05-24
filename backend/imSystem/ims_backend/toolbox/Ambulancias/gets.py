a678508827f1b6b946459b49e27f0d8e74c0fe79
luuuuuur <plazasjolifenzio@gmail.com>
2026-05-23 20:58:52 -0400

    INsumo id bug

--- backend/imSystem/ims_backend/toolbox/Ambulancias/gets.py
+++ backend/imSystem/ims_backend/toolbox/Ambulancias/gets.py
@@ -1,10 +1,10 @@
 from ims_backend.models import *
 from ims_backend.toolbox import exceptions
-from ims_backend.serializers import AmbulanciaSerializerID
+from ims_backend.serializers import InsumoIdSerializer
 from ims_backend.toolbox.Ambulancias import inventario
 def get_perid(request):
     if request.query_params:
-        serializer = AmbulanciaSerializerID(data=request.query_params)
+        serializer = InsumoIdSerializer(data=request.query_params)
         if serializer.is_valid():
             valid_data = serializer.validated_data
             r = inventario.specific(valid_data)
