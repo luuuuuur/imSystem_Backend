ad411e3cedba79728c4cadfdc82c544ea8bc48db
luuuuuur <plazasjolifenzio@gmail.com>
2026-06-10 19:54:52 -0400

    migrations bug

--- backend/imSystem/scripts/daily.sh
+++ backend/imSystem/scripts/daily.sh
@@ -55,8 +55,7 @@ echo "=== ACTUALIZANDO DEPENDENCIAS ==="
 "$PIP" install -r "$INSTALL_FILE" --quiet
 "$PIP" install "${APP_DIR}"/wheels/rustjson-*.whl --force-reinstall
 echo "=== APLICANDO MIGRACIONES ==="
-"$PYTHON" "${DJANGO_APP}/manage.py" makemigrations --noinput
-"$PYTHON" "${DJANGO_APP}/manage.py" migrate --noinput 
+"$PYTHON" "${DJANGO_APP}/manage.py" migrate --noinput
 
 echo "=== REINICIANDO GUNICORN Y NGINX Y CELERY ==="
 cp ~/product/imSystem_Backend/backend/ims_test_client.html ~/product/imSystem_Backend/backend/imSystem/staticfiles/
