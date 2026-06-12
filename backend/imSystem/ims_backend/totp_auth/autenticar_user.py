e6bf09e770c962e8297903e8551acf179f88f3f9
luuuuuur <plazasjolifenzio@gmail.com>
2026-06-12 15:39:24 -0400

    unauth bug

--- backend/imSystem/ims_backend/totp_auth/autenticar_user.py
+++ backend/imSystem/ims_backend/totp_auth/autenticar_user.py
@@ -1,13 +1,13 @@
 from django.contrib.auth import authenticate, login
-from ims_backend.toolbox.exceptions import UnauthorizedException
+from ims_backend.toolbox.exceptions import UnAuthorizedException
 from ims_backend.totp_auth.authentication import authentication
 from ims_backend.models import Personal
 def autenticar(request, data):
     user = authenticate(username=data["username"], password=data["password"])
     if user is None:
-        raise UnauthorizedException(detail="Credenciales incorrectas")
+        raise UnAuthorizedException(detail="Credenciales incorrectas")
     if not user.is_active:
-        raise UnauthorizedException(detail="Usuario inactivo")
+        raise UnAuthorizedException(detail="Usuario inactivo")
     request.session['pre_auth_user_id'] = user.id
     return True
 
@@ -15,7 +15,7 @@ def iniciar_sesion(request, data):
     user_id = request.session.get('pre_auth_user_id')
     user_data = Personal.objects.get(id=user_id)
     if not authentication(user_data.totp_secret, data["totp_code"]):
-        raise UnauthorizedException(detail="Código TOTP incorrecto")
+        raise UnAuthorizedException(detail="Código TOTP incorrecto")
     
     login(request, user_data)
     del request.session['pre_auth_user_id']
